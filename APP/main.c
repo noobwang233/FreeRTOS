70607c01054a059dabf5d21562424d325989587f
noobwang233 <1213236870@qq.com>
2024-04-11 18:06:45 +0800

    init

--- APP/main.c
+++ APP/main.c
@@ -1,149 +1,218 @@
-/*!
-    \file    main.c
-    \brief   led spark with systick, USART print and key example
+// /**
+//   ******************************************************************************
+//   * @file    Project/STM32F10x_StdPeriph_Template/main.c 
+//   * @author  MCD Application Team
+//   * @version V3.5.0
+//   * @date    08-April-2011
+//   * @brief   Main program body
+//   ******************************************************************************
+//   * @attention
+//   *
+//   * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+//   * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
+//   * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
+//   * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
+//   * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
+//   * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+//   *
+//   * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
+//   ******************************************************************************
+//   */  
 
-    \version 2017-02-10, V1.0.0, firmware for GD32F30x
-    \version 2018-10-10, V1.1.0, firmware for GD32F30x
-    \version 2018-12-25, V2.0.0, firmware for GD32F30x
-    \version 2020-09-30, V2.1.0, firmware for GD32F30x 
-*/
+// /* Includes ------------------------------------------------------------------*/
+#include "stm32f10x.h"
+// // #include "stm32_eval.h"
+// #include <stdio.h>
 
-/*
-    Copyright (c) 2020, GigaDevice Semiconductor Inc.
-
-    Redistribution and use in source and binary forms, with or without modification, 
-are permitted provided that the following conditions are met:
-
-    1. Redistributions of source code must retain the above copyright notice, this 
-       list of conditions and the following disclaimer.
-    2. Redistributions in binary form must reproduce the above copyright notice, 
-       this list of conditions and the following disclaimer in the documentation 
-       and/or other materials provided with the distribution.
-    3. Neither the name of the copyright holder nor the names of its contributors 
-       may be used to endorse or promote products derived from this software without 
-       specific prior written permission.
-
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
-IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
-INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
-NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
-PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
-WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
-OF SUCH DAMAGE.
-*/
-
-#include "gd32e50x.h"
-#include "gd32e503z_eval.h"
-#include "gd32e503z_lcd_eval.h"
-#include "FreeRTOS.h"
-#include "task.h"
-#include <stdint.h>
-#include <stdio.h>
-
-TaskHandle_t LED1_Task_Handle = NULL; /* 任务句柄 */
-TaskHandle_t LED2_Task_Handle = NULL;
-TaskHandle_t TaskCreate_Handle = NULL;
-
-static void LED1_Task(void* parameter)
+int main()
 {
-    char_format_struct type1 = {CHAR_FONT_8_16, CHAR_DIRECTION_HORIZONTAL, LCD_COLOR_BLACK, LCD_COLOR_WHITE};
-    while (1)
-    {
-        gd_eval_led_on(LED1);
-        vTaskDelay(pdMS_TO_TICKS(500)); /* 延时500 ms */
-        gd_eval_led_off(LED1);
-        vTaskDelay(pdMS_TO_TICKS(500)); /* 延时500 ms */
-        lcd_char_display(10,10, 'A', type1);
-    }
+  return 0;
 }
+// // #ifdef USE_STM32100B_EVAL
+// //  #include "stm32100b_eval_lcd.h"
+// // #elif defined USE_STM3210B_EVAL
+// //  #include "stm3210b_eval_lcd.h"
+// // #elif defined USE_STM3210E_EVAL
+// //  #include "stm3210e_eval_lcd.h" 
+// // #elif defined USE_STM3210C_EVAL
+// //  #include "stm3210c_eval_lcd.h"
+// // #elif defined USE_STM32100E_EVAL
+// //  #include "stm32100e_eval_lcd.h"
+// // #endif
+
+// /** @addtogroup STM32F10x_StdPeriph_Template
+//   * @{
+//   */
+
+// // /* Private typedef -----------------------------------------------------------*/
+// // /* Private define ------------------------------------------------------------*/
+// // #ifdef USE_STM32100B_EVAL
+// //   #define MESSAGE1   "STM32 MD Value Line " 
+// //   #define MESSAGE2   " Device running on  " 
+// //   #define MESSAGE3   "  STM32100B-EVAL    " 
+// // #elif defined (USE_STM3210B_EVAL)
+// //   #define MESSAGE1   "STM32 Medium Density" 
+// //   #define MESSAGE2   " Device running on  " 
+// //   #define MESSAGE3   "   STM3210B-EVAL    " 
+// // #elif defined (STM32F10X_XL) && defined (USE_STM3210E_EVAL)
+// //   #define MESSAGE1   "  STM32 XL Density  " 
+// //   #define MESSAGE2   " Device running on  " 
+// //   #define MESSAGE3   "   STM3210E-EVAL    "
+// // #elif defined (USE_STM3210E_EVAL)
+// //   #define MESSAGE1   " STM32 High Density " 
+// //   #define MESSAGE2   " Device running on  " 
+// //   #define MESSAGE3   "   STM3210E-EVAL    " 
+// // #elif defined (USE_STM3210C_EVAL)
+// //   #define MESSAGE1   " STM32 Connectivity " 
+// //   #define MESSAGE2   " Line Device running" 
+// //   #define MESSAGE3   " on STM3210C-EVAL   "
+// // #elif defined (USE_STM32100E_EVAL)
+// //   #define MESSAGE1   "STM32 HD Value Line " 
+// //   #define MESSAGE2   " Device running on  " 
+// //   #define MESSAGE3   "  STM32100E-EVAL    "   
+// // #endif
+
+// // /* Private macro -------------------------------------------------------------*/
+// // /* Private variables ---------------------------------------------------------*/
+// //  USART_InitTypeDef USART_InitStructure;
+
+// // /* Private function prototypes -----------------------------------------------*/
+// // #ifdef __GNUC__
+// // /* With GCC/RAISONANCE, small printf (option LD Linker->Libraries->Small printf
+// //    set to 'Yes') calls __io_putchar() */
+// // #define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+// // #else
+// // #define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+// // #endif /* __GNUC__ */
+
+// /* Private functions ---------------------------------------------------------*/
+
+// /**
+//   * @brief  Main program.
+//   * @param  None
+//   * @retval None
+//   */
+// int main(void)
+// {
+//   /*!< At this stage the microcontroller clock setting is already configured, 
+//        this is done through SystemInit() function which is called from startup
+//        file (startup_stm32f10x_xx.s) before to branch to application main.
+//        To reconfigure the default setting of SystemInit() function, refer to
+//        system_stm32f10x.c file
+//      */     
+
+//   /* Initialize LEDs, Key Button, LCD and COM port(USART) available on
+//      STM3210X-EVAL board ******************************************************/
+//   STM_EVAL_LEDInit(LED1);
+//   STM_EVAL_LEDInit(LED2);
+//   STM_EVAL_LEDInit(LED3);
+//   STM_EVAL_LEDInit(LED4);
+
+//   /* USARTx configured as follow:
+//         - BaudRate = 115200 baud  
+//         - Word Length = 8 Bits
+//         - One Stop Bit
+//         - No parity
+//         - Hardware flow control disabled (RTS and CTS signals)
+//         - Receive and transmit enabled
+//   */
+//   USART_InitStructure.USART_BaudRate = 115200;
+//   USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+//   USART_InitStructure.USART_StopBits = USART_StopBits_1;
+//   USART_InitStructure.USART_Parity = USART_Parity_No;
+//   USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+//   USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+
+//   STM_EVAL_COMInit(COM1, &USART_InitStructure);
+
+//   /* Initialize the LCD */
+// #ifdef USE_STM32100B_EVAL
+//   STM32100B_LCD_Init();
+// #elif defined (USE_STM3210B_EVAL)
+//   STM3210B_LCD_Init();
+// #elif defined (USE_STM3210E_EVAL)
+//   STM3210E_LCD_Init();
+// #elif defined (USE_STM3210C_EVAL)
+//   STM3210C_LCD_Init();
+// #elif defined (USE_STM32100E_EVAL)
+//   STM32100E_LCD_Init();  
+// #endif
+
+//   /* Display message on STM3210X-EVAL LCD *************************************/
+//   /* Clear the LCD */ 
+//   LCD_Clear(LCD_COLOR_WHITE);
+
+//   /* Set the LCD Back Color */
+//   LCD_SetBackColor(LCD_COLOR_BLUE);
+//   /* Set the LCD Text Color */
+//   LCD_SetTextColor(LCD_COLOR_WHITE);
+//   LCD_DisplayStringLine(LCD_LINE_0, (uint8_t *)MESSAGE1);
+//   LCD_DisplayStringLine(LCD_LINE_1, (uint8_t *)MESSAGE2);
+//   LCD_DisplayStringLine(LCD_LINE_2, (uint8_t *)MESSAGE3);
+
+//   /* Retarget the C library printf function to the USARTx, can be USART1 or USART2
+//      depending on the EVAL board you are using ********************************/
+//   printf("\n\r %s", MESSAGE1);
+//   printf(" %s", MESSAGE2);
+//   printf(" %s\n\r", MESSAGE3);
+
+//   /* Turn on leds available on STM3210X-EVAL **********************************/
+//   STM_EVAL_LEDOn(LED1);
+//   STM_EVAL_LEDOn(LED2);
+//   STM_EVAL_LEDOn(LED3);
+//   STM_EVAL_LEDOn(LED4);
+
+//   /* Add your application code here
+//      */
+
+//   /* Infinite loop */
+//   while (1)
+//   {
+//   }
+// }
+
+// /**
+//   * @brief  Retargets the C library printf function to the USART.
+//   * @param  None
+//   * @retval None
+//   */
+// PUTCHAR_PROTOTYPE
+// {
+//   /* Place your implementation of fputc here */
+//   /* e.g. write a character to the USART */
+//   USART_SendData(EVAL_COM1, (uint8_t) ch);
+
+//   /* Loop until the end of transmission */
+//   while (USART_GetFlagStatus(EVAL_COM1, USART_FLAG_TC) == RESET)
+//   {}
+
+//   return ch;
+// }
+
+// #ifdef  USE_FULL_ASSERT
+
+// /**
+//   * @brief  Reports the name of the source file and the source line number
+//   *         where the assert_param error has occurred.
+//   * @param  file: pointer to the source file name
+//   * @param  line: assert_param error line source number
+//   * @retval None
+//   */
+// void assert_failed(uint8_t* file, uint32_t line)
+// { 
+//   /* User can add his own implementation to report the file name and line number,
+//      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+//   /* Infinite loop */
+//   while (1)
+//   {
+//   }
+// }
+// #endif
+
+// /**
+//   * @}
+//   */
 
 
-static void LED2_Task(void* parameter)
-{
-    char_format_struct type1 = {CHAR_FONT_8_16, CHAR_DIRECTION_HORIZONTAL, LCD_COLOR_BLACK, LCD_COLOR_WHITE};
-    while (1)
-    {
-        gd_eval_led_on(LED2);
-        vTaskDelay(pdMS_TO_TICKS(800)); /* 延时800 ms */
-        gd_eval_led_off(LED2);
-        vTaskDelay(pdMS_TO_TICKS(800)); /* 延时800 ms */
-        lcd_char_display(10,30, 'B', type1);
-    }
-}
-
-static void TaskCreate_Task(void* parameter)
-{
-    BaseType_t xReturn = pdPASS;/* 定义一个创建信息返回值，默认为pdPASS */
-
-    taskENTER_CRITICAL(); //进入临界区
-
-    xReturn = xTaskCreate(  (TaskFunction_t )LED1_Task, /* 任务入口函数 */
-                                (const char* )"LED1_Task",/* 任务名字 */
-                            (uint16_t )512, /* 任务栈大小 */
-                            (void* )NULL, /* 任务入口函数参数 */
-                            (UBaseType_t )2, /* 任务的优先级 */
-                            (TaskHandle_t* )&LED1_Task_Handle);/* 任务句柄指针 */
-    if (pdPASS == xReturn)
-    {
-        printf("Create LED1_Task successfully!\r\n");
-    }
-    else
-    {
-        printf("Create LED1_Task failed!\r\n");
-    }
-
-    xReturn = xTaskCreate(  (TaskFunction_t )LED2_Task, /* 任务入口函数 */
-                                (const char* )"LED2_Task",/* 任务名字 */
-                            (uint16_t )512, /* 任务栈大小 */
-                            (void* )NULL, /* 任务入口函数参数 */
-                            (UBaseType_t )2, /* 任务的优先级 */
-                            (TaskHandle_t* )&LED2_Task_Handle);/* 任务句柄指针 */
-    if (pdPASS == xReturn)
-    {
-        printf("Create LED2_Task successfully!\r\n");
-    }
-    else
-    {
-        printf("Create LED2_Task failed!\r\n");
-    }
-
-    vTaskDelete(TaskCreate_Handle); //删除TaskCreate 任务
-
-    taskEXIT_CRITICAL(); //退出临界区
-}
-
-int main(void)
-{
-    BaseType_t xReturn = pdPASS;/* 定义一个创建信息返回值，默认为pdPASS */
-
-    gd_eval_led_init(LED1);
-    gd_eval_led_init(LED2);
-    gd_eval_com_init(EVAL_COM0);
-    exmc_lcd_init();
-    lcd_init();
-
-    xReturn = xTaskCreate(  (TaskFunction_t )TaskCreate_Task, /* 任务入口函数 */
-                                (const char* )"TaskCreate_Task",/* 任务名字 */
-                            (uint16_t )128, /* 任务栈大小 */
-                            (void* )NULL, /* 任务入口函数参数 */
-                            (UBaseType_t )3, /* 任务的优先级 */
-                            (TaskHandle_t* )&TaskCreate_Handle);/* 任务句柄指针 */
-    if (pdPASS == xReturn)
-        vTaskStartScheduler(); /* 启动任务，开启调度 */
-
-    while (1){}
-}
-
-/* retarget the C library printf function to the USART */
-int _write (int fd, char *pBuffer, int size)  
-{  
-    for (int i = 0; i < size; i++)  
-    {  
-		usart_data_transmit(EVAL_COM0, (uint8_t)pBuffer[i]);
-		while(RESET == usart_flag_get(EVAL_COM0, USART_FLAG_TBE));		
-    }
-    return size;  
-}
+// /******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
