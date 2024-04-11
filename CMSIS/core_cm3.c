a60530c5f906114c4461a2c5458ca8cd50deb5a7
noobwang233 <1213236870@qq.com>
2024-04-11 19:52:51 +0800

    save

--- CMSIS/core_cm3.c
+++ CMSIS/core_cm3.c
@@ -392,7 +392,7 @@ uint32_t __LDREXW(uint32_t *addr)
  */
 uint32_t __STREXB(uint8_t value, uint8_t *addr)
 {
-  __ASM("strexb r0, r0, [r1]");
+  __ASM volatile ("strexb %0, %2, [%1]" : "=&r" (result) : "r" (addr), "r" (value) );//yiming add 2012.04.09
   __ASM("bx lr");
 }
 
@@ -407,7 +407,7 @@ uint32_t __STREXB(uint8_t value, uint8_t *addr)
  */
 uint32_t __STREXH(uint16_t value, uint16_t *addr)
 {
-  __ASM("strexh r0, r0, [r1]");
+  __ASM volatile ("strexh %0, %2, [%1]" : "=&r" (result) : "r" (addr), "r" (value) );//yiming add 2012.04.09
   __ASM("bx lr");
 }
 
