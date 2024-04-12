de19053a673a83bf009138762e4d6bc21b310231
APAC\WTA3WX <Tao.WANG3@cn.bosch.com>
2024-04-12 14:32:12 +0800

    finish init FreeRTOS of STM32F103

--- APP/main.c
+++ APP/main.c
@@ -1,108 +1,96 @@
 #include "stm32f10x.h"
 #include "misc.h"
 #include "stm32f103_atk.h"
-// #include "FreeRTOS.h"
-// #include "task.h"
+#include "FreeRTOS.h"
+#include "task.h"
 #include <stdio.h>
 
-// TaskHandle_t LED1_Task_Handle = NULL; /* 任务句柄 */
-// TaskHandle_t LED2_Task_Handle = NULL;
-// TaskHandle_t TaskCreate_Handle = NULL;
+TaskHandle_t LED1_Task_Handle = NULL; /* 任务句柄 */
+TaskHandle_t LED2_Task_Handle = NULL;
+TaskHandle_t TaskCreate_Handle = NULL;
 
-// static void LED1_Task(void* parameter)
-// {
-//     while (1)
-//     {
-//         board_led_on(LED0);
-//         vTaskDelay(pdMS_TO_TICKS(500)); /* 延时500 ms */
-//         board_led_off(LED0);
-//         vTaskDelay(pdMS_TO_TICKS(500)); /* 延时500 ms */
-//     }
-// }
-
-
-// static void LED2_Task(void* parameter)
-// {
-//     while (1)
-//     {
-//         board_led_on(LED1);
-//         vTaskDelay(pdMS_TO_TICKS(800)); /* 延时800 ms */
-//         board_led_off(LED1);
-//         vTaskDelay(pdMS_TO_TICKS(800)); /* 延时800 ms */
-//     }
-// }
-
-// static void TaskCreate_Task(void* parameter)
-// {
-//     BaseType_t xReturn = pdPASS;/* 定义一个创建信息返回值，默认为pdPASS */
-
-//     taskENTER_CRITICAL(); //进入临界区
-
-//     xReturn = xTaskCreate(  (TaskFunction_t )LED1_Task, /* 任务入口函数 */
-//                                 (const char* )"LED1_Task",/* 任务名字 */
-//                             (uint16_t )512, /* 任务栈大小 */
-//                             (void* )NULL, /* 任务入口函数参数 */
-//                             (UBaseType_t )2, /* 任务的优先级 */
-//                             (TaskHandle_t* )&LED1_Task_Handle);/* 任务句柄指针 */
-//     if (pdPASS == xReturn)
-//     {
-//         printf("Create LED1_Task successfully!\r\n");
-//     }
-//     else
-//     {
-//         printf("Create LED1_Task failed!\r\n");
-//     }
-
-//     xReturn = xTaskCreate(  (TaskFunction_t )LED2_Task, /* 任务入口函数 */
-//                                 (const char* )"LED2_Task",/* 任务名字 */
-//                             (uint16_t )512, /* 任务栈大小 */
-//                             (void* )NULL, /* 任务入口函数参数 */
-//                             (UBaseType_t )2, /* 任务的优先级 */
-//                             (TaskHandle_t* )&LED2_Task_Handle);/* 任务句柄指针 */
-//     if (pdPASS == xReturn)
-//     {
-//         printf("Create LED2_Task successfully!\r\n");
-//     }
-//     else
-//     {
-//         printf("Create LED2_Task failed!\r\n");
-//     }
-
-//     vTaskDelete(TaskCreate_Handle); //删除TaskCreate 任务
-
-//     taskEXIT_CRITICAL(); //退出临界区
-// }
+static void LED1_Task(void* parameter)
+{
+    while (1)
+    {
+        board_led_on(LED0);
+        vTaskDelay(pdMS_TO_TICKS(500)); /* 延时500 ms */
+        board_led_off(LED0);
+        vTaskDelay(pdMS_TO_TICKS(500)); /* 延时500 ms */
+    }
+}
 
 
- void Delay(u32 count)
- {
-   u32 i=0;
-   for(;i<count;i++);
- }
+static void LED2_Task(void* parameter)
+{
+    while (1)
+    {
+        board_led_on(LED1);
+        vTaskDelay(pdMS_TO_TICKS(800)); /* 延时800 ms */
+        board_led_off(LED1);
+        vTaskDelay(pdMS_TO_TICKS(800)); /* 延时800 ms */
+    }
+}
+
+static void TaskCreate_Task(void* parameter)
+{
+    BaseType_t xReturn = pdPASS;/* 定义一个创建信息返回值，默认为pdPASS */
+
+    taskENTER_CRITICAL(); //进入临界区
+
+    xReturn = xTaskCreate(  (TaskFunction_t )LED1_Task, /* 任务入口函数 */
+                                (const char* )"LED1_Task",/* 任务名字 */
+                            (uint16_t )512, /* 任务栈大小 */
+                            (void* )NULL, /* 任务入口函数参数 */
+                            (UBaseType_t )2, /* 任务的优先级 */
+                            (TaskHandle_t* )&LED1_Task_Handle);/* 任务句柄指针 */
+    if (pdPASS == xReturn)
+    {
+        printf("Create LED1_Task successfully!\r\n");
+    }
+    else
+    {
+        printf("Create LED1_Task failed!\r\n");
+    }
+
+    xReturn = xTaskCreate(  (TaskFunction_t )LED2_Task, /* 任务入口函数 */
+                                (const char* )"LED2_Task",/* 任务名字 */
+                            (uint16_t )512, /* 任务栈大小 */
+                            (void* )NULL, /* 任务入口函数参数 */
+                            (UBaseType_t )2, /* 任务的优先级 */
+                            (TaskHandle_t* )&LED2_Task_Handle);/* 任务句柄指针 */
+    if (pdPASS == xReturn)
+    {
+        printf("Create LED2_Task successfully!\r\n");
+    }
+    else
+    {
+        printf("Create LED2_Task failed!\r\n");
+    }
+
+    vTaskDelete(TaskCreate_Handle); //删除TaskCreate 任务
+
+    taskEXIT_CRITICAL(); //退出临界区
+}
+
 int main()
 {
-    // BaseType_t xReturn = pdPASS;/* 定义一个创建信息返回值，默认为pdPASS */
-    // NVIC_SetPriorityGrouping(NVIC_PriorityGroup_2);
+    BaseType_t xReturn = pdPASS;/* 定义一个创建信息返回值，默认为pdPASS */
+    NVIC_SetPriorityGrouping(NVIC_PriorityGroup_4);
     board_led_init(LED0);
     board_led_init(LED1);
     board_com_init(115200);
 
-    // xReturn = xTaskCreate(  (TaskFunction_t )TaskCreate_Task, /* 任务入口函数 */
-    //                             (const char* )"TaskCreate_Task",/* 任务名字 */
-    //                         (uint16_t )128, /* 任务栈大小 */
-    //                         (void* )NULL, /* 任务入口函数参数 */
-    //                         (UBaseType_t )3, /* 任务的优先级 */
-    //                         (TaskHandle_t* )&TaskCreate_Handle);/* 任务句柄指针 */
-    // if (pdPASS == xReturn)
-    //     vTaskStartScheduler(); /* 启动任务，开启调度 */
+    xReturn = xTaskCreate(  (TaskFunction_t )TaskCreate_Task, /* 任务入口函数 */
+                                (const char* )"TaskCreate_Task",/* 任务名字 */
+                            (uint16_t )128, /* 任务栈大小 */
+                            (void* )NULL, /* 任务入口函数参数 */
+                            (UBaseType_t )3, /* 任务的优先级 */
+                            (TaskHandle_t* )&TaskCreate_Handle);/* 任务句柄指针 */
+    if (pdPASS == xReturn)
+        vTaskStartScheduler(); /* 启动任务，开启调度 */
 
-    while (1)
-    {
-        board_led_on(LED1);
-        Delay(50000);
-        board_led_off(LED1);
-        Delay(50000);
-    }
+    while (1){}
 
 
     return 0;
--- FreeRTOS/FreeRTOSConfig.h
+++ FreeRTOS/FreeRTOSConfig.h
@@ -1,167 +1,118 @@
-/*
- * FreeRTOS V202212.01
- * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- * https://www.FreeRTOS.org
- * https://github.com/FreeRTOS
- *
- */
-
-/******************************************************************************
-	See http://www.freertos.org/a00110.html for an explanation of the
-	definitions contained in this file.
-******************************************************************************/
-
 #ifndef FREERTOS_CONFIG_H
 #define FREERTOS_CONFIG_H
 
-/*-----------------------------------------------------------
- * Application specific definitions.
- *
- * These definitions should be adjusted for your particular hardware and
- * application requirements.
- *
- * THESE PARAMETERS ARE DESCRIBED WITHIN THE 'CONFIGURATION' SECTION OF THE
- * FreeRTOS API DOCUMENTATION AVAILABLE ON THE FreeRTOS.org WEB SITE.
- * https://www.FreeRTOS.org/a00110.html
- *----------------------------------------------------------*/
-
+//针对不同的编译器调用不同的stdint.h 文件
+#if defined(__ICCARM__) || defined(__CC_ARM) || defined(__GNUC__)
+#include <stdint.h>
 extern uint32_t SystemCoreClock;
+#endif
 
-/* Cortex M33 port configuration. */
-#define configENABLE_MPU								0
-#define configENABLE_FPU								1
-#define configENABLE_TRUSTZONE							0
+//断言
+#define configASSERT_DEFINED                             1
+extern int	printf (const char *__restrict, ...);
+#define vAssertCalled(char,int) printf("Error:%s,%d\r\n",char,int)
+#define configASSERT( x )    if( ( x ) == 0 ) vAssertCalled(__FILE__,__LINE__)
+#define configQUEUE_REGISTRY_SIZE                        20
 
-/* Constants related to the behaviour or the scheduler. */
-#define configUSE_PORT_OPTIMISED_TASK_SELECTION			0
-#define configUSE_PREEMPTION							1
-#define configUSE_TIME_SLICING							1
-#define configMAX_PRIORITIES							( 5 )
-#define configIDLE_SHOULD_YIELD							1
-#define configUSE_16_BIT_TICKS							0 /* Only for 8 and 16-bit hardware. */
+#define configUSE_PREEMPTION                             1
+#define configUSE_TIME_SLICING                           1
+/* 某些运行FreeRTOS 的硬件有两种方法选择下一个要执行的任务：
+ * 通用方法和特定于硬件的方法（以下简称“特殊方法”）。
+ *
+ * 通用方法：
+ * 1.configUSE_PORT_OPTIMISED_TASK_SELECTION 为 0 或者硬件不支持这种特殊方法。
+ * 2.可以用于所有FreeRTOS 支持的硬件
+ * 3.完全用C 实现，效率略低于特殊方法。
+ * 4.不强制要求限制最大可用优先级数目
+ * 特殊方法：
+ * 1.必须将configUSE_PORT_OPTIMISED_TASK_SELECTION 设置为1。
+ * 2.依赖一个或多个特定架构的汇编指令（一般是类似计算前导零[CLZ]指令）。
+ * 3.比通用方法更高效
+ * 4.一般强制限定最大可用优先级数目为32
+ 一般是硬件计算前导零指令，如果所使用的，MCU 没有这些硬件指令的话此宏应该设置为0！
+ */
+#define configUSE_PORT_OPTIMISED_TASK_SELECTION          1
 
-/* Constants that describe the hardware and memory usage. */
-#define configCPU_CLOCK_HZ								SystemCoreClock
-#define configMINIMAL_STACK_SIZE						( ( uint16_t ) 128 )
-#define configMINIMAL_SECURE_STACK_SIZE					( 1024 )
-#define configMAX_TASK_NAME_LEN							( 12 )
-#define configTOTAL_HEAP_SIZE							( ( size_t ) ( 50 * 1024 ) )
+#define configUSE_IDLE_HOOK                              0
+#define configUSE_TICK_HOOK                              0
+#define configUSE_DAEMON_TASK_STARTUP_HOOK               0
+#define configUSE_MALLOC_FAILED_HOOK                     0
+#define configCHECK_FOR_STACK_OVERFLOW                   0
 
-/* Constants that build features in or out. */
-#define configUSE_MUTEXES								1
-#define configUSE_TICKLESS_IDLE							1
-#define configUSE_APPLICATION_TASK_TAG					0
-#define configUSE_NEWLIB_REENTRANT						0
-#define configUSE_COUNTING_SEMAPHORES					1
-#define configUSE_RECURSIVE_MUTEXES						1
-#define configUSE_QUEUE_SETS							0
-#define configUSE_TASK_NOTIFICATIONS					1
-#define configUSE_TRACE_FACILITY						1
+#define configGENERATE_RUN_TIME_STATS                    0
 
-/* Constants that define which hook (callback) functions should be used. */
-#define configUSE_IDLE_HOOK								0
-#define configUSE_TICK_HOOK								0
-#define configUSE_MALLOC_FAILED_HOOK					0
+#define configCPU_CLOCK_HZ                               (SystemCoreClock)
+#define configTICK_RATE_HZ                               ( ( TickType_t ) 1000 )
 
-/* Constants provided for debugging and optimisation assistance. */
-#define configCHECK_FOR_STACK_OVERFLOW					0
-#define configASSERT( x )								if( ( x ) == 0 ) { taskDISABLE_INTERRUPTS(); for( ;; ); }
-#define configQUEUE_REGISTRY_SIZE						0
+//可使用的最大优先级
+#define configMAX_PRIORITIES                             (5)
 
-/* Software timer definitions. */
-#define configUSE_TIMERS								1
-#define configTIMER_TASK_PRIORITY						( 3 )
-#define configTIMER_QUEUE_LENGTH						5
-#define configTIMER_TASK_STACK_DEPTH					( configMINIMAL_STACK_SIZE  )
+//空闲任务使用的堆栈大小
+#define configMINIMAL_STACK_SIZE ((unsigned short)128)
+
+//任务名字字符串长度
+#define configMAX_TASK_NAME_LEN                          ( 10 )
+//系统节拍计数器变量数据类型，1 表示为16 位无符号整形，0 表示为32 位无符号整形
+#define configUSE_16_BIT_TICKS 0
+
+#define configTOTAL_HEAP_SIZE                            ( ( size_t ) ( 36*1024 ) )
+
+#define configUSE_TRACE_FACILITY                         1
+//空闲任务放弃CPU 使用权给其他同优先级的用户任务
+#define configIDLE_SHOULD_YIELD                          1
+
+//启用队列
+#define configUSE_QUEUE_SETS                             1
+#define configUSE_TASK_NOTIFICATIONS                     1
+
+#define configTIMER_QUEUE_LENGTH                         10
+#define configTIMER_TASK_PRIORITY                        ( configMAX_PRIORITIES - 1 )
+#define configUSE_COUNTING_SEMAPHORES                    1
+#define configSUPPORT_DYNAMIC_ALLOCATION                 1
+#define configSUPPORT_STATIC_ALLOCATION                  0
+#define configNUM_TX_DESCRIPTORS                        15
+#define configSTREAM_BUFFER_TRIGGER_LEVEL_TEST_MARGIN    2
 
 /* Set the following definitions to 1 to include the API function, or zero
- * to exclude the API function.  NOTE:  Setting an INCLUDE_ parameter to 0 is
- * only necessary if the linker does not automatically remove functions that are
- * not referenced anyway. */
-#define INCLUDE_vTaskPrioritySet						1
-#define INCLUDE_uxTaskPriorityGet						1
-#define INCLUDE_vTaskDelete								1
-#define INCLUDE_vTaskCleanUpResources					0
-#define INCLUDE_vTaskSuspend							1
-#define INCLUDE_vTaskDelayUntil							1
-#define INCLUDE_vTaskDelay								1
-#define INCLUDE_uxTaskGetStackHighWaterMark				0
-#define INCLUDE_xTaskGetIdleTaskHandle					0
-#define INCLUDE_eTaskGetState							1
-#define INCLUDE_xTaskResumeFromISR						0
-#define INCLUDE_xTaskGetCurrentTaskHandle				1
-#define INCLUDE_xTaskGetSchedulerState					0
-#define INCLUDE_xSemaphoreGetMutexHolder				0
-#define INCLUDE_xTimerPendFunctionCall					1
+ * to exclude the API function. */
 
-/* This demo makes use of one or more example stats formatting functions.  These
- * format the raw data provided by the uxTaskGetSystemState() function in to
- * human readable ASCII form.  See the notes in the implementation of vTaskList()
- * within FreeRTOS/Source/tasks.c for limitations. */
-#define configUSE_STATS_FORMATTING_FUNCTIONS			1
 
-/* Dimensions a buffer that can be used by the FreeRTOS+CLI command interpreter.
- * See the FreeRTOS+CLI documentation for more information:
- * https://www.FreeRTOS.org/FreeRTOS-Plus/FreeRTOS_Plus_CLI/ */
-#define configCOMMAND_INT_MAX_OUTPUT_SIZE				2048
+#define configUSE_MUTEXES                         1
+#define configUSE_RECURSIVE_MUTEXES               1
+#define configUSE_TIMERS                          1
+#define configUSE_COUNTING_SEMAPHORES             1
+#define configTIMER_TASK_STACK_DEPTH              ( configMINIMAL_STACK_SIZE * 2 )
 
-/* Interrupt priority configuration follows...................... */
+#define INCLUDE_vTaskPrioritySet                  1
+#define INCLUDE_uxTaskPriorityGet                 1
+#define INCLUDE_vTaskDelete                       1
+#define INCLUDE_vTaskCleanUpResources             0
+#define INCLUDE_vTaskSuspend                      1
+#define INCLUDE_vTaskDelayUntil                   1
+#define INCLUDE_vTaskDelay                        1
+#define INCLUDE_uxTaskGetStackHighWaterMark       1
+#define INCLUDE_uxTaskGetStackHighWaterMark2      1
+#define INCLUDE_xTaskGetSchedulerState            1
+#define INCLUDE_xTimerGetTimerDaemonTaskHandle    1
+#define INCLUDE_xTaskGetIdleTaskHandle            1
+#define INCLUDE_xTaskGetHandle                    1
+#define INCLUDE_eTaskGetState                     1
+#define INCLUDE_xSemaphoreGetMutexHolder          1
+#define INCLUDE_xTimerPendFunctionCall            1
+#define INCLUDE_xTaskAbortDelay                   1
 
-/* Use the system definition, if there is one. */
 #ifdef __NVIC_PRIO_BITS
 	#define configPRIO_BITS								__NVIC_PRIO_BITS
 #else
-	#define configPRIO_BITS								3	 /* 8 priority levels. */
+	#define configPRIO_BITS								4	 /* 8 priority levels. */
 #endif
-
-/* The lowest interrupt priority that can be used in a call to a "set priority"
- * function. */
-#define configLIBRARY_LOWEST_INTERRUPT_PRIORITY			0x07
-
-/* The highest interrupt priority that can be used by any interrupt service
- * routine that makes calls to interrupt safe FreeRTOS API functions.  DO NOT
- * CALL INTERRUPT SAFE FREERTOS API FUNCTIONS FROM ANY INTERRUPT THAT HAS A
- * HIGHER PRIORITY THAN THIS! (higher priorities are lower numeric values). */
+#define configLIBRARY_LOWEST_INTERRUPT_PRIORITY			0x15
 #define configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY	5
-
-/* Interrupt priorities used by the kernel port layer itself.  These are generic
- * to all Cortex-M ports, and do not rely on any particular library functions. */
 #define configKERNEL_INTERRUPT_PRIORITY					( configLIBRARY_LOWEST_INTERRUPT_PRIORITY << ( 8 - configPRIO_BITS ) )
-
-/* !!!! configMAX_SYSCALL_INTERRUPT_PRIORITY must not be set to zero !!!!
- * See http://www.FreeRTOS.org/RTOS-Cortex-M3-M4.html. */
 #define configMAX_SYSCALL_INTERRUPT_PRIORITY			( configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY << ( 8 - configPRIO_BITS ) )
 
-/* The #ifdef guards against the file being included from IAR assembly files. */
-#ifndef __IASMARM__
+#define xPortPendSVHandler PendSV_Handler
+#define vPortSVCHandler SVC_Handler
+#define xPortSysTickHandler SysTick_Handler
 
-	/* Constants related to the generation of run time stats. */
-	#define configGENERATE_RUN_TIME_STATS				0
-	#define portCONFIGURE_TIMER_FOR_RUN_TIME_STATS()
-	#define portGET_RUN_TIME_COUNTER_VALUE()			0
-	#define configTICK_RATE_HZ							( ( TickType_t ) 100 )
-
-#endif /* __IASMARM__ */
-
-/* Enable static allocation. */
-// #define configSUPPORT_STATIC_ALLOCATION					1
-#define configSUPPORT_DYNAMIC_ALLOCATION                1
 #endif /* FREERTOS_CONFIG_H */
